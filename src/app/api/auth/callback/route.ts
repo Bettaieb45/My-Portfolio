840c5f18a3fbe99c2cede2e6a430690b0d855f10
aziz <mohamedazizbettaieb6@edu.bme.hu>
2025-01-20 18:13:54 +0100

    aded debugging to callback

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,11 +2,11 @@ import { google } from 'googleapis';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest): Promise<Response> {
-  try {
-    console.log('GMAIL_CLIENT_ID:', process.env.GMAIL_CLIENT_ID);
-    console.log('GMAIL_CLIENT_SECRET:', process.env.GMAIL_CLIENT_SECRET);
-    console.log('GMAIL_REDIRECT_URI:', process.env.GMAIL_REDIRECT_URI);
+  console.log('GMAIL_CLIENT_ID:', process.env.GMAIL_CLIENT_ID);
+  console.log('GMAIL_CLIENT_SECRET:', process.env.GMAIL_CLIENT_SECRET);
+  console.log('GMAIL_REDIRECT_URI:', process.env.GMAIL_REDIRECT_URI);
 
+  try {
     console.log('Starting token exchange...');
     const url = new URL(req.url);
     const code = url.searchParams.get('code');
