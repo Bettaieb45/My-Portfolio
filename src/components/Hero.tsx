2cc0d1c80822cd6befed769aa78028979d02e5e7
aziz <mohamedazizbettaieb6@edu.bme.hu>
2025-01-21 12:24:30 +0100

    fixing the styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ const Hero = () => {
     <div className="hero container-1120 pt-32 pb-16  " id="hero">
       <div className="hero-wrapper">
         <h1>Hi, I&apos;m Aziz!</h1>
-        <h2 className="max-w-xl py-8">
+        <h3 className="max-w-xl py-8">
           Junior Web Developer Specializing in Modern Web Technologies
-        </h2>
+        </h3>
         <div className="button-wrapper flex flex-col gap-5 items-center sm:flex-row  max-w-xl justify-between pointer-events-auto">
           <CtaButton text="View My Work" href="#portfolio" />
           <CtaButton text="Get In Touch" href="#contact" />
